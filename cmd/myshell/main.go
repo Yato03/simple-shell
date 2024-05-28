d491c2856084d66ccf6436d90fb1d7249403d632
yato03 <miguelhs3523@gmail.com>
2024-05-28 21:33:58 +0200

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,7 +12,7 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		bufio.NewReader(os.Stdin).ReadString('\n')
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
 		// Print the input
 		fmt.Println(input + ": command not found")
