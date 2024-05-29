0639fcd1aa9c072a073ecda47008b00a84cd7575
Yato03 <miguelhs3523@gmail.com>
2024-05-29 15:17:58 +0200

    pass stage

---
+++ cmd/myshell/commands.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Command interface {
+	execute(args []string)
+	getName() string
+}
+
+type ExitCommand struct {
+	name string
+}
+
+func (c *ExitCommand) execute(args []string) {
+	if len(args) < 1 {
+		os.Exit(0)
+	}
+	exitCode, err := strconv.Atoi(args[0])
+	if err != nil {
+		os.Exit(0)
+	}
+	os.Exit(exitCode)
+}
+
+func (c *ExitCommand) getName() string {
+	return c.name
+}
+
+type EchoCommand struct {
+	name string
+}
+
+func (c *EchoCommand) execute(args []string) {
+	message := strings.Join(args, " ")
+	fmt.Println(message)
+}
+
+func (c *EchoCommand) getName() string {
+	return c.name
+}
+
+type TypeCommand struct {
+	name string
+}
+
+func (c *TypeCommand) execute(args []string) {
+	if len(args) < 1 {
+		fmt.Println("type: missing argument")
+		return
+	}
+
+	commandName := args[0]
+
+	//search builint commands
+	for _, value := range commands {
+		if value.getName() == commandName {
+			fmt.Println(commandName + " is a shell builtin")
+			return
+		}
+	}
+
+	//search in PATH
+	path, ok := os.LookupEnv("PATH")
+
+	if ok {
+		dirs := strings.Split(path, ":")
+		for _, dir := range dirs {
+			if listFiles(dir, commandName) {
+				fmt.Println(commandName + " is " + dir + "/" + commandName)
+				return
+			}
+		}
+	}
+
+	fmt.Println(commandName + " not found")
+
+}
+
+func (c *TypeCommand) getName() string {
+	return c.name
+}
+
+var commands = []Command{
+	&ExitCommand{name: "exit"},
+	&EchoCommand{name: "echo"},
+	&TypeCommand{name: "type"},
+}
--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,94 +5,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 )
-
-type Command interface {
-	execute(args []string)
-	getName() string
-}
-
-type ExitCommand struct {
-	name string
-}
-
-func (c *ExitCommand) execute(args []string) {
-	if len(args) < 1 {
-		os.Exit(0)
-	}
-	exitCode, err := strconv.Atoi(args[0])
-	if err != nil {
-		os.Exit(0)
-	}
-	os.Exit(exitCode)
-}
-
-func (c *ExitCommand) getName() string {
-	return c.name
-}
-
-type EchoCommand struct {
-	name string
-}
-
-func (c *EchoCommand) execute(args []string) {
-	message := strings.Join(args, " ")
-	fmt.Println(message)
-}
-
-func (c *EchoCommand) getName() string {
-	return c.name
-}
-
-type TypeCommand struct {
-	name string
-}
-
-func (c *TypeCommand) execute(args []string) {
-	if len(args) < 1 {
-		fmt.Println("type: missing argument")
-		return
-	}
-
-	commandName := args[0]
-
-	//search builint commands
-	for _, value := range commands {
-		if value == commandName {
-			fmt.Println(commandName + " is a shell builtin")
-			return
-		}
-	}
-
-	//search in PATH
-	path, ok := os.LookupEnv("PATH")
-
-	if ok {
-		dirs := strings.Split(path, ":")
-		for _, dir := range dirs {
-			if listFiles(dir, commandName) {
-				fmt.Println(commandName + " is " + dir + "/" + commandName)
-				return
-			}
-		}
-	}
-
-	fmt.Println(commandName + " not found")
-
-}
-
-func (c *TypeCommand) getName() string {
-	return c.name
-}
-
-var commands = []string{"exit", "echo", "type"}
-var commands2 = []Command{
-	&ExitCommand{name: "exit"},
-	&EchoCommand{name: "echo"},
-	&TypeCommand{name: "type"},
-}
 
 func listFiles(dir string, name string) bool {
 	result := false
@@ -121,7 +35,7 @@ func execCommand(command []string) {
 	commandName := command[0]
 	args := command[1:]
 
-	for _, c := range commands2 {
+	for _, c := range commands {
 		if c.getName() == commandName {
 			c.execute(args)
 			return
