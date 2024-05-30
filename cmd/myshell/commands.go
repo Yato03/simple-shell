e1e8bfb9cae55dd0659da0a6bac36fe934859eff
Yato03 <miguelhs3523@gmail.com>
2024-05-30 21:16:08 +0200

    pass stage

--- cmd/myshell/commands.go
+++ cmd/myshell/commands.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -83,14 +85,49 @@ type PwdCommand struct {
 	name string
 }
 
+/*
 func (c *PwdCommand) execute(args []string) {
 	path, ok := os.LookupEnv("PWD")
 	if ok {
 		fmt.Println(path)
 	}
+}*/
+
+func (c *PwdCommand) execute(args []string) {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(path)
 }
 
 func (c *PwdCommand) getName() string {
+	return c.name
+}
+
+type ChangeDirectoryCommand struct {
+	name string
+}
+
+func (c *ChangeDirectoryCommand) execute(args []string) {
+
+	if len(args) < 1 {
+		fmt.Println("cd: missing argument")
+		return
+	}
+
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println("cd: cannot access the current path")
+		return
+	}
+	err = os.Chdir(filepath.Join(path, args[0]))
+	if err != nil {
+		fmt.Println("cd: " + path + ": No such file or directory")
+	}
+}
+
+func (c *ChangeDirectoryCommand) getName() string {
 	return c.name
 }
 
@@ -99,4 +136,5 @@ var commands = []Command{
 	&EchoCommand{name: "echo"},
 	&TypeCommand{name: "type"},
 	&PwdCommand{name: "pwd"},
+	&ChangeDirectoryCommand{name: "cd"},
 }
